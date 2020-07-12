a213b2dd774277910ec3d2306cf0abbc58530649
baydarov <egbaydarov@edu.hse.ru>
2020-07-12 05:27:52 +0300

    add two index point gesture

---
+++ Assets/Scripts/PointGesture.cs
@@ -0,0 +1,77 @@
+﻿using LeapInternal;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PointGesture : MonoBehaviour
+{
+    public UnityEvent OnActivate;
+    public UnityEvent OnDeactivate;
+
+    void Start()
+    {
+        
+    }
+
+    bool leftIndex = false;
+    bool rightIndex = false;
+
+    bool isActive = false;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void SampleMethod()
+    {
+        Debug.Log("HEllo");
+    }
+
+    public void SampleMethod1()
+    {
+        Debug.Log("BB");
+    }
+
+    public void lActivate()
+    {
+        leftIndex = true;
+        if (rightIndex && !isActive)
+        {
+            isActive = true;
+            OnActivate.Invoke();
+        }
+    }
+
+    public void lDeactivate()
+    {
+        leftIndex = false;
+        if (!rightIndex && isActive)
+        {
+            isActive = false;
+            OnDeactivate.Invoke();
+        }
+    }
+
+    public void rActivate()
+    {
+        rightIndex = true;
+        if (leftIndex && !isActive)
+        {
+            isActive = true;
+            OnActivate.Invoke();
+        }
+    }
+
+    public void rDeactivate()
+    {
+        rightIndex = false;
+        if (!leftIndex && isActive)
+        {
+            isActive = false;
+            OnDeactivate.Invoke();
+        }
+    }
+}
