350a900fab21189b20bc05f3a0c4e1ef29b8354f
baydarov <egbaydarov@edu.hse.ru>
2020-07-13 18:33:58 +0300

    add bascket with collision events (MAinMEnu scene)

---
+++ Assets/Scripts/BallsColission.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BallsColission : MonoBehaviour
+{
+    public UnityEvent OnSmallBallEnter;
+    public UnityEvent OnBigBallEnter;
+    public UnityEvent OnMiddleSmallBallEnter;
+    public UnityEvent OnMiddleBigBallEnter;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.name)
+        {
+            case "big":
+                OnBigBallEnter.Invoke();
+                break;
+            case "small":
+                OnSmallBallEnter.Invoke();
+                break;
+            case "middlesmall":
+                OnMiddleSmallBallEnter.Invoke();
+                break;
+            case "middlebig":
+                OnMiddleBigBallEnter.Invoke();
+                break;
+            default:
+                break;
+        }
+    }
+}
---
+++ Assets/Scripts/ReturnToDefaultPlace.cs
@@ -0,0 +1,42 @@
+﻿using Leap.Unity.Interaction;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReturnToDefaultPlace : MonoBehaviour
+{
+    Vector3 defaultPos;
+    InteractionBehaviour ib;
+    Rigidbody rb;
+
+    float timer = 0.0f;
+
+    void Start()
+    {
+        defaultPos = transform.position;
+    }
+
+
+    void Update()
+    {
+        if (transform.position != defaultPos && !ib.isGrasped)
+            timer += Time.deltaTime;
+        else
+            timer = 0;
+
+        if (timer % 60 >= 3)
+        {
+            timer = 0f;
+
+            transform.position = defaultPos;
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+        }
+    }
+
+    private void Awake()
+    {
+        ib = GetComponent<InteractionBehaviour>();
+        rb = GetComponent<Rigidbody>();
+    }
+}
