1c7a932b0fda851ee8805cbbcfec39a4a0908357
baydarov <egbaydarov@edu.hse.ru>
2020-07-14 14:46:53 +0300

    upd train and main scene

---
+++ Assets/Scripts/EntryProcessing.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowTrainWords : MonoBehaviour
+{
+    List<string> words;
+
+    public Text tm;
+    public TMP_InputField TMP_if;
+    public GameObject confirmButton;
+    public GameObject sentenceField;
+
+    string[] data = {"abd","abv"};
+
+    int current;
+
+    void Start()
+    {
+        words = new List<string>(data);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        tm.text = words[current];
+
+        if (String.IsNullOrEmpty(TMP_if.text))
+        {
+            confirmButton.SetActive(false);
+            sentenceField.SetActive(true);
+        }
+        else
+        {
+            confirmButton.SetActive(true);
+            sentenceField.SetActive(false);
+        }
+    }
+
+    public void inc_current()
+    {
+        if (current + 1 > words.Count - 1)
+            current = 0;
+        else
+            ++current;
+    }
+
+    public void dec_current()
+    {
+        if (current - 1 < 0)
+            current = words.Count - 1;
+        else
+            --current;
+    }
+}
---
+++ Assets/Scripts/TrainTextEntryProcessing.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class TrainTextEntryProcessing : MonoBehaviour
+{
+
+    public GameObject sentenceField;
+    public GameObject confirmButton;
+    public TMP_InputField TMP_if;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (string.IsNullOrEmpty(TMP_if.text))
+        {
+            sentenceField.SetActive(true);
+            confirmButton.SetActive(false);
+        }
+        else
+        {
+            sentenceField.SetActive(false);
+            confirmButton.SetActive(true);
+        }
+        
+    }
+}
