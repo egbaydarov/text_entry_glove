505ad67cb26d457719b40d3bb8c9de0b4c251fd5
Pavel Manakhov <pavel.manakhov@gmail.com>
2020-07-17 18:42:21 +0300

    Implemented sound feedback

---
+++ Assets/Scripts/SoundOnHighlight.cs
@@ -0,0 +1,36 @@
+﻿using LeapMotionGesture;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SoundOnHighlight : MonoBehaviour, IPointerEnterHandler
+{
+    [SerializeField]
+    private new AudioSource audio;
+    [SerializeField]
+    private AudioClip soundToPlay;
+
+    private void Start()
+    {
+        if (audio == null)
+        {
+            Debug.LogError("SoundOnHighlight: The 'Audio' field cannot be left unassigned. Disabling the script");
+            enabled = false;
+            return;
+        }
+
+        if (soundToPlay == null)
+        {
+            Debug.LogError("SoundOnHighlight: The 'SoundToPlay' field cannot be left unassigned. Disabling the script");
+            enabled = false;
+            return;
+        }
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (!enabled || AirStrokeMapper.pinchIsOn)
+            return;
+
+        audio.PlayOneShot(soundToPlay);
+    }
+}
