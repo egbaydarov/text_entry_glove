876860a709c6233393ea9a928d2c198793425af9
baydarov <egbaydarov@edu.hse.ru>
2020-07-19 04:46:28 +0300

    add smoothness to DirectHandRay scene ponter

---
+++ Assets/Scripts/MoveDirectPointer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveDirectPointer : MonoBehaviour
+{
+    public Transform wrist;
+    public Transform palm;
+    public float smoothing;
+    public float rotationSmoothing;
+
+    public Vector3 offset;
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, (wrist.position + palm.position) / 2, smoothing * Time.deltaTime);
+        transform.rotation = Quaternion.Lerp(transform.rotation, palm.rotation * Quaternion.Euler(offset), Time.deltaTime * rotationSmoothing);
+    }
+}
