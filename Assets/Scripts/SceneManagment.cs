7cb1777767307412e43b21d3dcbdc557c560ec04
Pavel Manakhov <pavel.manakhov@gmail.com>
2020-07-23 11:18:26 +0300

    Made the 'Exit' feature stop a playing mode also

--- Assets/Scripts/SceneManagment.cs
+++ Assets/Scripts/SceneManagment.cs
@@ -10,7 +10,11 @@ public class SceneManagment : MonoBehaviour
    
     public void Exit()
     {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
         Application.Quit();
+#endif
     }
 
     public void Train()
