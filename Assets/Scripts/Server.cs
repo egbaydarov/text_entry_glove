6bfde7408dc3a86926683de1cd64ae63eaf4a7d8
baydarov <egbaydarov@edu.hse.ru>
2020-07-24 18:50:19 +0300

    add do not destroy for server.cs

--- Assets/Scripts/Server.cs
+++ Assets/Scripts/Server.cs
@@ -70,7 +70,7 @@ public class Server : MonoBehaviour
     {
         go = GameObject.Find("keyboard");
         shift = go.GetComponent<Shift>();
-        im = GetComponent<Image>();
+        im = go.GetComponent<Image>();
         
 
         NetworkSetup();
@@ -308,5 +308,17 @@ public class Server : MonoBehaviour
         IsBroadcasting = false;
         Listener.Stop();
         udpSocket.Close();
+    }
+
+    private void Awake()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("Server");
+
+        if (objs.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+
+        DontDestroyOnLoad(this.gameObject);
     }
 }
