8bba70a4bcccaec4c94f6c58d9c8312da6caad83
eetrukhan <eetrukhan@edu.hse.ru>
2020-10-05 20:23:53 +0300

    first try

--- Assets/Scripts/Screenshot.cs
+++ Assets/Scripts/Screenshot.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+﻿/*using System.Collections;
 using System.Collections.Generic;
 using System.Drawing;
 using UnityEngine;
@@ -43,3 +43,4 @@ public class Screenshot : MonoBehaviour
         return sprite;
     }
 }
+*/
